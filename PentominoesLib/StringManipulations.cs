aff2a27653b707f4469de14878e815b94f696187
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-05-14 13:20:15 +0100

    Add TODO list to readme

--- PentominoesLib/StringManipulations.cs
+++ PentominoesLib/StringManipulations.cs
@@ -9,17 +9,17 @@ namespace PentominoesLib
         public static IEnumerable<string> RotateStrings(IEnumerable<string> strings)
         {
             var stringsList = strings.ToList();
+            var reversedStrings = ReflectStrings(strings).ToArray();
             var w = stringsList[0].Length;
             var h = stringsList.Count;
-            var reversedStrings = ReflectStrings(strings).ToArray();
             var xs = Enumerable.Range(0, w);
             var ys = Enumerable.Range(0, h);
-            return xs.Select(x => new String(ys.Select(y => reversedStrings[y][x]).ToArray()));
+            return xs.Select(x => new string(ys.Select(y => reversedStrings[y][x]).ToArray()));
         }
 
         public static IEnumerable<string> ReflectStrings(IEnumerable<string> strings)
         {
-            return strings.Select(s => new String(s.ToArray().Reverse().ToArray()));
+            return strings.Select(s => new string(s.ToArray().Reverse().ToArray()));
         }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+# TODO
+
+* De-dup the 520 solutions to 65 solutions
+  * i.e. ignore solutions that are just rotations or transpositions of other solutions
+* Implement `DrawSolution` functionally (currently, it mutates a 2D array of strings)
+* Use immutable collections throughout
+
 # Links
 
 * https://en.wikipedia.org/wiki/Pentomino
