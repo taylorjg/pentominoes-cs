062ea740e82001fce8f96fd823a7cc6e95709ba6
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-05-14 16:34:25 +0100

    Implement DrawSolution functionally

--- PentominoesApp/Program.cs
+++ PentominoesApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using PentominoesLib;
 using DlxLib;
 
@@ -19,26 +20,54 @@ namespace PentominoesApp
             DrawSolution(rows, solution);
         }
 
+        // const uniqueSolutions = []
+        // const uniqueJoinedGrids = []
+
+        // const isSolutionUnique = (rows, solution) => {
+        //   const formattedSolution1 = formatSolution(rows, solution)
+        //   const formattedSolution2 = M.rotateStrings(formattedSolution1)
+        //   const formattedSolution3 = M.rotateStrings(formattedSolution2)
+        //   const formattedSolution4 = M.rotateStrings(formattedSolution3)
+        //   const formattedSolution5 = M.reflectStrings(formattedSolution1)
+        //   const formattedSolution6 = M.reflectStrings(formattedSolution2)
+        //   const formattedSolution7 = M.reflectStrings(formattedSolution3)
+        //   const formattedSolution8 = M.reflectStrings(formattedSolution4)
+        //   const joinedGrids = [
+        //     formattedSolution1.join('|'),
+        //     formattedSolution2.join('|'),
+        //     formattedSolution3.join('|'),
+        //     formattedSolution4.join('|'),
+        //     formattedSolution5.join('|'),
+        //     formattedSolution6.join('|'),
+        //     formattedSolution7.join('|'),
+        //     formattedSolution8.join('|')
+        //   ]
+        //   return R.intersection(joinedGrids, uniqueJoinedGrids).length === 0
+        // }        
+
         private static void DrawSolution(IEnumerable<Placement> rows, Solution solution)
         {
-            var cells = new string[8, 8];
-            cells[3, 3] = " ";
-            cells[3, 4] = " ";
-            cells[4, 3] = " ";
-            cells[4, 4] = " ";
-            foreach (var rowIndex in solution.RowIndexes)
+            var rowsArray = rows.ToImmutableArray();
+            var seed = ImmutableArray.Create<(int x, int y, string label)>(
+                (3, 3, " "),
+                (3, 4, " "),
+                (4, 3, " "),
+                (4, 4, " ")
+            );
+            var cellsArray = solution.RowIndexes.Aggregate(seed, (accOuter, rowIndex) =>
             {
-                var placement = rows.ElementAt(rowIndex);
-                foreach (var coords in placement.Variation.Coords)
+                var placement = rowsArray[rowIndex];
+                return placement.Variation.Coords.Aggregate(accOuter, (accInner, coords) =>
                 {
                     var x = placement.Location.X + coords.X;
                     var y = placement.Location.Y + coords.Y;
-                    cells[y, x] = placement.Piece.Label;
-                }
-            }
-            var lines = Enumerable.Range(0, 8).Select(rowIndex =>
-                string.Join("", Enumerable.Range(0, 8).Select(colIndex =>
-                    cells[rowIndex, colIndex])));
+                    return accInner.Add((x, y, placement.Piece.Label));
+                });
+            });
+            var lines =
+                from y in Enumerable.Range(0, 8)
+                let row = Enumerable.Range(0, 8).Select(x => cellsArray.First(t => t.x == x && t.y == y))
+                select string.Join("", row.Select(t => t.label));
             foreach (var line in lines) Console.WriteLine(line);
             Console.WriteLine(new string('-', 80));
         }
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# TODO
+# TODO
 
 * De-dup the 520 solutions to 65 solutions
   * i.e. ignore solutions that are just rotations or transpositions of other solutions
-* Implement `DrawSolution` functionally (currently, it mutates a 2D array of strings)
+* ~~Implement `DrawSolution` functionally (currently, it mutates a 2D array of strings)~~
 * Use immutable collections throughout
 
 # Links
